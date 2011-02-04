db3c7dde32bfae8b2fae2e2ccc440cb9c800ac85
Jeremy Keiper <jeremy@openmrs.org>
2011-02-04 22:10:06 +0000

    REST Module - changed patient service methods to getPatients

--- web/src/org/openmrs/module/restmodule/web/FindPatientResource.java
+++ web/src/org/openmrs/module/restmodule/web/FindPatientResource.java
@@ -1,3 +1,16 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.restmodule.web;
 
 import java.io.IOException;
@@ -10,8 +23,6 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.openmrs.Patient;
 import org.openmrs.api.context.Context;
-import org.openmrs.module.restmodule.RestUtil;
-import org.openmrs.module.restmodule.XmlPatient;
 
 /**
  * Provides RESTful access to the patient search facilities of OpenMRS
@@ -21,9 +32,9 @@ public class FindPatientResource implements RestResource {
 	/**
 	 * Handle all requests to this patient search resource
 	 */
-	public void handleRequest(Operation operation, OutputType outputType, String restRequest,
-			HttpServletRequest request, HttpServletResponse response)
-			throws ServletException, IOException {
+	public void handleRequest(Operation operation, OutputType outputType,
+			String restRequest, HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
 
 		PrintWriter out = response.getWriter();
 
@@ -31,10 +42,10 @@ public class FindPatientResource implements RestResource {
 
 		case GET:
 			List<Patient> patientList = Context.getPatientService()
-					.findPatients(restRequest, false);
+					.getPatients(restRequest);
 
 			PatientResource.printPatientList(out, outputType, patientList);
-			
+
 			break;
 
 		case POST:
