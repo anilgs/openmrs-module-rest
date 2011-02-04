aef8a9c109f4d3213e16726ff0ecd798306118d9
Jeremy Keiper <jeremy@openmrs.org>
2011-02-04 22:08:13 +0000

    REST Module - added license docs to several classes

--- src/org/openmrs/module/restmodule/RestUtil.java
+++ src/org/openmrs/module/restmodule/RestUtil.java
@@ -1,9 +1,24 @@
+/**
+ * The contents of this file are subject to the OpenMRS Public License
+ * Version 1.0 (the "License"); you may not use this file except in
+ * compliance with the License. You may obtain a copy of the License at
+ * http://license.openmrs.org
+ *
+ * Software distributed under the License is distributed on an "AS IS"
+ * basis, WITHOUT WARRANTY OF ANY KIND, either express or implied. See the
+ * License for the specific language governing rights and limitations
+ * under the License.
+ *
+ * Copyright (C) OpenMRS, LLC.  All Rights Reserved.
+ */
 package org.openmrs.module.restmodule;
 
 import java.util.Date;
 import java.util.List;
 import java.util.Vector;
 
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.openmrs.api.context.Context;
 import org.openmrs.api.context.ContextAuthenticationException;
 
@@ -38,6 +53,8 @@ public class RestUtil {
 	 */
 	public static final String REST_API_PRIVILEGE = "Access REST API";
 
+	private static final Log log = LogFactory.getLog(RestUtil.class);
+	
 	private static Object lock = new Object(); // used for synchronizing
 	private static List<String[]> allowedIpList = null;
 	private static long propertiesUpdatedTime = 0;
@@ -74,6 +91,7 @@ public class RestUtil {
 		try {
 			Context.authenticate(username, password);
 		} catch (ContextAuthenticationException e) {
+			log.error("could not authenticate user " + username);
 		}
 		return Context.isAuthenticated() && Context.hasPrivilege(REST_API_PRIVILEGE);
 	}
